691e4121faa5ebec0e44b41bb253c350c20f02db
Pro-Pinina <levantamientoinformacion8@gmail.com>
2019-07-15 15:37:33 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Route} from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+import { EscritorioComponent } from './escritorio/escritorio.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
+
+const routes: Route[]= [
+  {path: '',component: InicioComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'escritorio', component:EscritorioComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    LoginComponent,
+    EscritorioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
